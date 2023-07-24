0040b5db37ed3b11e2434b5271dd4f7b3bb00999
OdaharaSakura <117719725+OdaharaSakura@users.noreply.github.com>
2023-07-24 14:48:43 +0900

    タイトル素材追加＋スクリプト追加

---
+++ Yaeeeeeeeeeh/Assets/S.Odahara/ChangeScene.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ChangeScene : MonoBehaviour
+{
+
+    //遷移先のシーン
+    public string nextScene;
+
+    //時間経過でシーン遷移する場合
+    public bool bTimeFlag;
+
+    public static string loadAfterScene;
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        if (bTimeFlag)
+        {
+            Invoke("Change", 3.0f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+       
+    }
+
+    //シーン切り替え
+    public void Change()
+    {
+        //ロード後のシーン
+        loadAfterScene = nextScene;
+        SceneManager.LoadScene("Load");
+    }
+}
