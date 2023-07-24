d4bb8ff40e0456712ff6b34a60da97fd3760e3b8
VICTUS_230614\otsjh <otsj.haltky2021@gmail.com>
2023-07-24 13:21:47 +0900

    13:21 07/24 add InputSystem

---
+++ Yaeeeeeeeeeh/Assets/H.Otsj/Script/SCR_RaycastAround.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_RaycastAround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitData;
+
+        for(float rot = 0; rot < 2 * Mathf.PI; rot += Mathf.PI / 180.0f){
+            Vector3 dir;
+            dir.x = Mathf.Cos(rot);
+            dir.y = Mathf.Tan(rot);
+            dir.z = Mathf.Sin(rot);
+
+            Physics.Raycast(transform.position, dir * 10);
+            Debug.DrawRay(transform.position, dir * 10, Color.blue, 0.01f, true);
+            
+
+
+        }
+    }
+}
