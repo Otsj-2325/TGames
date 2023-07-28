c08b0a6ca169b063e99576e4661295b19c7ebd27
MizukiTakanashi <mizuki.takanashi11@gmail.com>
2023-07-28 17:24:16 +0900

    円の影から円柱作る途中です

--- Yaeeeeeeeeeh/Assets/Takanashi/CreateShadowCircle.cs
+++ Yaeeeeeeeeeh/Assets/Takanashi/CreateShadowCircle.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshFilter))]
 
-public class RatCastFromCamera : MonoBehaviour
+public class CreateShadowCircle : MonoBehaviour
 {
-    [Header("キューブ")]
-    [SerializeField] private GameObject cube;
+    [Header("球体")]
+    [SerializeField] private GameObject circle;
 
     private MeshFilter mesh_filter;
     private Mesh mesh;
@@ -18,24 +18,30 @@ public class RatCastFromCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mesh_filter = cube.GetComponent<MeshFilter>();
+        mesh_filter = circle.GetComponent<MeshFilter>();
         mesh = mesh_filter.mesh;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Eキーを押したら影の形からオブジェクトを生成
+        //if (!Input.GetKeyDown(KeyCode.E))
+        //{
+        //    return;
+        //}
+
         Vector3[] vertices = mesh.vertices;
         List<Vector3> vertices3 = new List<Vector3>();
         Vector3[] vertices2 = mesh.vertices;        // キューブの場所と各頂点の座標を加算したもの
         List<float> point_y = new List<float>();    // Y座標を取得
 
         // 頂点のY座標を取得
-        for(int i = 0; i < vertices.Length; i++)
+        for (int i = 0; i < vertices.Length; i++)
         {
             vertices3.Add(vertices[i]);
 
-            vertices2[i] = cube.gameObject.transform.position + vertices[i];
+            vertices2[i] = circle.gameObject.transform.position + vertices[i];
             if (!point_y.Contains(vertices2[i].y))
             {
                 point_y.Add(vertices2[i].y);
@@ -43,7 +49,7 @@ public class RatCastFromCamera : MonoBehaviour
         }
 
         // いらない座標を削除(裏面等)
-        for(int i = 0; i < point_y.Count; i++)
+        for (int i = 0; i < point_y.Count; i++)
         {
             List<int> right_max_index = new List<int>();  // 最も右側のベクトルのインデックス番号
             List<int> left_max_index = new List<int>();   // 最も左側のベクトルのインデックス番号
@@ -56,9 +62,9 @@ public class RatCastFromCamera : MonoBehaviour
                 angle[j] = -1.0f;
 
                 // 最も右側か左側の座標を求める(Y座標は同じ)
-                if(vertices2[j].y == point_y[i])
+                if (vertices2[j].y == point_y[i])
                 {
-                    Vector3 temp_straight = cube.gameObject.transform.position - gameObject.transform.position;
+                    Vector3 temp_straight = circle.gameObject.transform.position - gameObject.transform.position;
                     Vector3 temp_this = vertices2[j] - gameObject.transform.position;
                     float temp_inner = Vector3.Dot(temp_straight, temp_this);
                     float angleRad = Mathf.Acos(temp_inner / (temp_straight.magnitude * temp_this.magnitude));
@@ -70,7 +76,7 @@ public class RatCastFromCamera : MonoBehaviour
                     Vector3 temp_outer = Vector3.Cross(temp_straight, temp_this);
 
                     // 点が右側にある
-                    if(temp_outer.y > 0)
+                    if (temp_outer.y > 0)
                     {
                         if (right_max_index.Count == 0)
                         {
@@ -90,7 +96,8 @@ public class RatCastFromCamera : MonoBehaviour
                         }
                     }
                     // 点が左側にある
-                    else {
+                    else
+                    {
                         if (left_max_index.Count == 0)
                         {
                             left_angle = angle[j];
@@ -116,7 +123,7 @@ public class RatCastFromCamera : MonoBehaviour
             {
                 if (vertices2[j].y == point_y[i])
                 {
-                    if(!right_max_index.Contains(j) && !left_max_index.Contains(j))
+                    if (!right_max_index.Contains(j) && !left_max_index.Contains(j))
                     {
                         vertices3.Remove(vertices[j]);
                     }
@@ -124,14 +131,14 @@ public class RatCastFromCamera : MonoBehaviour
             }
         }
 
-        Mesh new_mesh = new Mesh();
-        var vertex = new List<Vector3>();
+        List<Vector3> vertex = new List<Vector3>();
+        List<float> vertex_x = new List<float>();
 
         // レイを生成し壁に当て、1影の座標を取得
         for (int i = 0; i < vertices3.Count; i++)
         {
             // レイの生成
-            Vector3 dir = cube.gameObject.transform.position - gameObject.transform.position;
+            Vector3 dir = circle.gameObject.transform.position - gameObject.transform.position;
             dir += vertices3[i];
             Ray ray = new Ray(gameObject.transform.position, dir);
             Debug.DrawRay(ray.origin, ray.direction * 30, Color.red, 0.1f); // 長さ３０、赤色で５秒間可視化
@@ -143,12 +150,13 @@ public class RatCastFromCamera : MonoBehaviour
                 if (hit.collider.CompareTag("Wall"))
                 {
                     vertex.Add(new Vector3(hit.point.x, hit.point.y, hit.point.z));
+                    vertex_x.Add(hit.point.x);
                 }
             }
         }
 
         Vector3 center_point = new Vector3();    // 影の中央座標
-        Vector3 dir_center = cube.gameObject.transform.position - gameObject.transform.position;
+        Vector3 dir_center = circle.gameObject.transform.position - gameObject.transform.position;
         Ray ray_center = new Ray(gameObject.transform.position, dir_center);
         RaycastHit[] hits_center = Physics.RaycastAll(ray_center);
         foreach (RaycastHit hit in hits_center)
@@ -160,117 +168,117 @@ public class RatCastFromCamera : MonoBehaviour
             }
         }
 
-        List<int> right_index = new List<int>();
-        List<int> left_index = new List<int>();
-        Vector3[] point = new Vector3[8];
-        for (int i = 0; i < vertex.Count; i++)
+        Mesh new_mesh = new Mesh();
+        //List<int> right_index = new List<int>();
+        //List<int> left_index = new List<int>();
+        Vector3[] point = new Vector3[vertex.Count * 2];
+
+        // X座標を右から見て、並べ替える(インデックスが丸と同じになるように)
+        vertex_x.Sort();
+        for(int i = vertex_x.Count - 1; i > -1; i--)
         {
-            // 左
-            if(vertex[i].x < center_point.x)
+            for(int j = 0; j < vertex.Count; j++)
             {
-                // 下
-                if(vertex[i].y < center_point.y)
+                if(vertex_x[i] == vertex[j].x)
                 {
-                    point[0] = vertex[i];
-                }
-                // 上
-                else
-                {
-                    point[2] = vertex[i];
+                    // 上半分
+                    if(center_point.y <= vertex[j].y)
+                    {
+
+                    }
+                    // 下半分
+                    else
+                    {
+
+                    }
                 }
             }
-            // 右
-            else
-            {
-                // 下
-                if (vertex[i].y < center_point.y)
-                {
-                    point[1] = vertex[i];
-                }
-                // 上
-                else
-                {
-                    point[3] = vertex[i];
-                }
-            }
+        }
+
+        point[0] = new Vector3(0.0f, 0.0f, 0.0f);
+
+        point[1] = new Vector3(1.0f, 0.0f, 0.0f);
+
+        for (int i = 0; i < 35; i++)
+        {
+            float rad = (i + 1) * 10 * Mathf.Deg2Rad;
+
+            point[36 - i].x = point[1].x * Mathf.Cos(rad) - point[1].y * Mathf.Sin(rad);
+            point[36 - i].y = -point[1].x * Mathf.Sin(rad) + point[1].y * Mathf.Cos(rad);
+
+            Debug.Log(point[i + 2].x);
+            Debug.Log(point[i + 2].y);
         }
 
         // 立体にする
-        for(int i = 0; i < 4; i++)
+        for (int i = 0; i < 37; i++)
         {
-            point[i + 4] = new Vector3(point[i].x, point[i].y, -0.1f);
+            point[i + 37] = new Vector3(point[i].x, point[i].y, -0.1f);
         }
         new_mesh.vertices = point;
 
-        //// インデックスを設定
-        //var indices = new List<int>();
-        //for (int i = 0; i < vertex.Count; i++)
-        //{
-        //    indices.Add(i);
-
-        //    if (i == vertex.Count - 1)
-        //    {
-        //        indices.Add(0);
-        //    }
-        //    else
-        //    {
-        //        indices.Add(i + 1);
-        //        //Debug.Log("index ：" + (i + 1));
-
-        //    }
-        //    //Debug.Log("index ：" + i);
-        //}
-
         // 頂点インデックスを設定（立方体の場合）
-        int[] triangles = new int[]
+        int[] triangles = new int[36 * 2 * 3 + 72 * 3];
+
+        // 表面
+        for (int i = 0; i < 35; i++)
         {
-            // 前面
-            0, 1, 2, // 1つ目の三角形
-            1, 3, 2, // 2つ目の三角形
+            triangles[i * 3] = 0;
+            triangles[i * 3 + 1] = i + 2;
+            triangles[i * 3 + 2] = i + 1;
+        }
+        triangles[35 * 3] = 0;
+        triangles[35 * 3 + 1] = 1;
+        triangles[35 * 3 + 2] = 36;
 
-            // 背面
-            5, 4, 6, // 1つ目の三角形
-            5, 6, 7, // 2つ目の三角形
+        // 裏面
+        for (int i = 0; i < 35; i++)
+        {
+            triangles[(i + 36) * 3] = 37;
+            triangles[(i + 36) * 3 + 1] = 37 + i + 1;
+            triangles[(i + 36) * 3 + 2] = 37 + i + 2;
+        }
+        triangles[71 * 3] = 37;
+        triangles[71 * 3 + 1] = 36 + 37;
+        triangles[71 * 3 + 2] = 38;
 
-            // 上面
-            2, 3, 6, // 1つ目の三角形
-            3, 7, 6, // 2つ目の三角形
+        // 横面
+        for (int i = 1; i < 36; i++)
+        {
+            int temp_num = 70 + i * 2;
+            triangles[temp_num * 3] = i;
+            triangles[temp_num * 3 + 1] = i + 1;
+            triangles[temp_num * 3 + 2] = i + 38;
 
-            // 底面
-            0, 4, 1, // 1つ目の三角形
-            1, 4, 5, // 2つ目の三角形
+            triangles[(temp_num + 1) * 3] = i;
+            triangles[(temp_num + 1) * 3 + 1] = i + 38;
+            triangles[(temp_num + 1) * 3 + 2] = i + 37;
+        }
 
-            // 左面
-            4, 0, 6, // 1つ目の三角形
-            6, 0, 2, // 2つ目の三角形
+        triangles[(70 + 36 * 2) * 3] = 36;
+        triangles[(70 + 36 * 2) * 3 + 1] = 1;
+        triangles[(70 + 36 * 2) * 3 + 2] = 38;
 
-            // 右面
-            1, 5, 3, // 1つ目の三角形
-            5, 7, 3, // 2つ目の三角形
-        };
+        triangles[(70 + 36 * 2 + 1) * 3] = 36;
+        triangles[(70 + 36 * 2 + 1) * 3 + 1] = 38;
+        triangles[(70 + 36 * 2 + 1) * 3 + 2] = 37 + 36;
+
         new_mesh.triangles = triangles;
 
         // 法線を計算して設定
         new_mesh.RecalculateNormals();
 
-        if (create)
-        {
-            GameObject newObject = new GameObject("NewMeshObject");
+        GameObject newObject = new GameObject("NewMeshObject");
 
-            //new_mesh.SetIndices(indices, MeshTopology.Lines, 0);
+        //MeshFilterはshredMeshをMeshRendererに引き渡す。
+        MeshFilter mfilter = newObject.AddComponent<MeshFilter>();
+        mfilter.mesh = new_mesh;
 
-            //MeshFilterはshredMeshをMeshRendererに引き渡す。
-            MeshFilter mfilter = newObject.AddComponent<MeshFilter>();
-            mfilter.mesh = new_mesh;
+        // MeshRendererを追加してマテリアルをアタッチ
+        MeshRenderer meshRenderer = newObject.AddComponent<MeshRenderer>();
 
-            // MeshRendererを追加してマテリアルをアタッチ
-            MeshRenderer meshRenderer = newObject.AddComponent<MeshRenderer>();
-            // ここで適切なマテリアルをアタッチする
+        newObject.transform.position = Vector3.zero; // 必要な位置に変更する
 
-            // 新しいオブジェクトをシーンに追加
-            newObject.transform.position = Vector3.zero; // 必要な位置に変更する
-
-            create = false;
-        }
+        newObject.transform.localScale = new Vector3(1.0f, 1.0f, 3.0f);
     }
 }
