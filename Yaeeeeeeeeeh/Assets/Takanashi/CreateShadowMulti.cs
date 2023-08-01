0d634bdff99a5669053948cf2b7b4c10893c51ae
MizukiTakanashi <mizuki.takanashi11@gmail.com>
2023-08-01 16:01:16 +0900

    複数のオブジェクトの影を生成する途中です

--- Yaeeeeeeeeeh/Assets/Takanashi/CreateShadowMulti.cs
+++ Yaeeeeeeeeeh/Assets/Takanashi/CreateShadowMulti.cs
@@ -2,24 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(MeshFilter))]
-
-public class CreateShadowCircle : MonoBehaviour
+public class CreateShadowMulti : MonoBehaviour
 {
     [Header("実体化する物")]
     [SerializeField] private GameObject circle;
 
-    private MeshFilter mesh_filter;
-    private Mesh mesh;
+    //private MeshFilter mesh_filter;
+    //private Mesh mesh;
+    private List<MeshFilter> mesh_filter = new List<MeshFilter>();
+    private List<Mesh> mesh = new List<Mesh>();
 
     private bool create = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        mesh_filter = circle.GetComponent<MeshFilter>();
-        mesh = mesh_filter.mesh;
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag("LightingObject");
+
+        // メッシュを取得
+        foreach (GameObject obj in objectsWithTag)
+        {
+            mesh_filter.Add(obj.GetComponent<MeshFilter>());
+            mesh.Add(obj.GetComponent<MeshFilter>().mesh);
+        }
+
+        //mesh_filter = circle.GetComponent<MeshFilter>();
+        //mesh = mesh_filter.mesh;
     }
 
     // Update is called once per frame
@@ -31,22 +39,55 @@ public class CreateShadowCircle : MonoBehaviour
             return;
         }
 
-        Vector3[] vertices = mesh.vertices;
-        List<Vector3> vertices3 = new List<Vector3>();
-        Vector3[] vertices2 = mesh.vertices;        // キューブの場所と各頂点の座標を加算したもの
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag("LightingObject");
+        List<Vector3[]> vertices = new List<Vector3[]>();
+        List<Vector3[]> vertices2 = new List<Vector3[]>();
+        List<GameObject> gameObject2 = new List<GameObject>();
+        List<Vector3[]> vertices3 = new List<Vector3[]>();
         List<float> point_y = new List<float>();    // Y座標を取得
 
-        //頂点のY座標を取得
-        for (int i = 0; i < vertices.Length; i++)
+        foreach (Mesh temp_mesh in mesh)
         {
-            vertices3.Add(vertices[i]);
+            vertices.Add(temp_mesh.vertices);
+            vertices3.Add(temp_mesh.vertices);
 
-            vertices2[i] = circle.gameObject.transform.position + vertices[i];
-            if (!point_y.Contains(vertices2[i].y))
+            Vector3[] temp_vector3 = new Vector3[temp_mesh.vertices.Length];
+            foreach (GameObject obj in objectsWithTag)
             {
-                point_y.Add(vertices2[i].y);
+                if (obj.GetComponent<MeshFilter>().mesh == temp_mesh)
+                {
+                    for (int i = 0; i < temp_mesh.vertices.Length; i++)
+                    {
+                        temp_vector3[i] = temp_mesh.vertices[i] + obj.gameObject.transform.position;
+                        if (!point_y.Contains(temp_vector3[i].y))
+                        {
+                            point_y.Add(temp_vector3[i].y);
+                        }
+                    }
+                    gameObject2.Add(obj);
+                    break;
+                }
             }
+            vertices2.Add(temp_vector3);
+            
         }
+
+        //Vector3[] vertices = mesh.vertices;
+        //List<Vector3> vertices3 = new List<Vector3>();
+        //Vector3[] vertices2 = mesh.vertices;        // キューブの場所と各頂点の座標を加算したもの
+        //List<float> point_y = new List<float>();    // Y座標を取得
+
+        // 頂点のY座標を取得
+        //for (int i = 0; i < vertices.Length; i++)
+        //{
+        //    vertices3.Add(vertices[i]);
+
+        //    vertices2[i] = circle.gameObject.transform.position + vertices[i];
+        //    if (!point_y.Contains(vertices2[i].y))
+        //    {
+        //        point_y.Add(vertices2[i].y);
+        //    }
+        //}
 
         //===========================================
         // いらない座標を削除(裏面等)
@@ -58,78 +99,86 @@ public class CreateShadowCircle : MonoBehaviour
             float angle = 0.0f;
             float right_angle = -1.0f;
             float left_angle = -1.0f;
-            for (int j = 0; j < vertices2.Length; j++)
+            for (int j = 0; j < vertices2.Count; j++)
             {
-                angle = -1.0f;
-
-                // 最も右側か左側の座標を求める(Y座標は同じ)
-                if (vertices2[j].y == point_y[i])
+                for (int k = 0; k < vertices2[j].Length; k++)
                 {
-                    Vector3 temp_straight = circle.gameObject.transform.position - gameObject.transform.position;
-                    Vector3 temp_this = vertices2[j] - gameObject.transform.position;
-                    float temp_inner = Vector3.Dot(temp_straight, temp_this);
-                    float angleRad = Mathf.Acos(temp_inner / (temp_straight.magnitude * temp_this.magnitude));
-                    float angleDeg = angleRad * Mathf.Rad2Deg;
-                    angle = angleDeg;
+                    angle = -1.0f;
 
-                    temp_straight.y = 0.0f;
-                    temp_this.y = 0.0f;
-                    Vector3 temp_outer = Vector3.Cross(temp_straight, temp_this);
-
-                    // 点が右側にある
-                    if (temp_outer.y > 0)
+                    // 最も右側か左側の座標を求める(Y座標は同じ)
+                    if (vertices2[j][k].y == point_y[i])
                     {
-                        if (right_max_index.Count == 0)
-                        {
-                            right_angle = angle;
-                            right_max_index.Add(j);
-                        }
+                        Vector3 temp_straight = gameObject2[j].gameObject.transform.position - gameObject.transform.position;
+                        Vector3 temp_this = vertices2[j][k] - gameObject.transform.position;
+                        float temp_inner = Vector3.Dot(temp_straight, temp_this);
+                        float angleRad = Mathf.Acos(temp_inner / (temp_straight.magnitude * temp_this.magnitude));
+                        float angleDeg = angleRad * Mathf.Rad2Deg;
+                        angle = angleDeg;
 
-                        if (right_angle < angle)
+                        temp_straight.y = 0.0f;
+                        temp_this.y = 0.0f;
+                        Vector3 temp_outer = Vector3.Cross(temp_straight, temp_this);
+
+                        // 点が右側にある
+                        if (temp_outer.y > 0)
                         {
-                            right_angle = angle;
-                            right_max_index.Clear();
-                            right_max_index.Add(j);
+                            if (right_max_index.Count == 0)
+                            {
+                                right_angle = angle;
+                                right_max_index.Add(k);
+                            }
+
+                            if (right_angle < angle)
+                            {
+                                right_angle = angle;
+                                right_max_index.Clear();
+                                right_max_index.Add(k);
+                            }
+                            else if (right_angle == angle)
+                            {
+                                right_max_index.Add(k);
+                            }
                         }
-                        else if (right_angle == angle)
+                        // 点が左側にある
+                        else
                         {
-                            right_max_index.Add(j);
+                            if (left_max_index.Count == 0)
+                            {
+                                left_angle = angle;
+                                left_max_index.Add(k);
+                            }
+
+                            if (left_angle < angle)
+                            {
+                                left_angle = angle;
+                                left_max_index.Clear();
+                                left_max_index.Add(k);
+                            }
+                            else if (left_angle == angle)
+                            {
+                                left_max_index.Add(k);
+                            }
                         }
                     }
-                    // 点が左側にある
-                    else
-                    {
-                        if (left_max_index.Count == 0)
-                        {
-                            left_angle = angle;
-                            left_max_index.Add(j);
-                        }
+                }
 
-                        if (left_angle < angle)
+                for (int k = 0; k < vertices2[j].Length; k++)
+                {
+                    if (vertices2[j][k].y == point_y[i])
+                    {
+                        if (!right_max_index.Contains(k) && !left_max_index.Contains(k))
                         {
-                            left_angle = angle;
-                            left_max_index.Clear();
-                            left_max_index.Add(j);
-                        }
-                        else if (left_angle == angle)
-                        {
-                            left_max_index.Add(j);
+                            //vertices3[j].;
                         }
                     }
                 }
             }
 
             // 同じY座標で最も右か左でなければ配列から削除する(その頂点は処理しない)
-            for (int j = 0; j < vertices2.Length; j++)
-            {
-                if (vertices2[j].y == point_y[i])
-                {
-                    if (!right_max_index.Contains(j) && !left_max_index.Contains(j))
-                    {
-                        vertices3.Remove(vertices[j]);
-                    }
-                }
-            }
+            //for (int j = 0; j < vertices2.Count; j++)
+            //{
+                
+            //}
         }
         //===========================================
 
@@ -143,7 +192,7 @@ public class CreateShadowCircle : MonoBehaviour
         {
             // レイの生成
             Vector3 dir = circle.gameObject.transform.position - gameObject.transform.position;
-            dir += vertices3[i];
+            //dir += vertices3[i];
             Ray ray = new Ray(gameObject.transform.position, dir);
             Debug.DrawRay(ray.origin, ray.direction * 30, Color.red, 0.1f); // 長さ３０、赤色で５秒間可視化
 
@@ -160,7 +209,7 @@ public class CreateShadowCircle : MonoBehaviour
         }
 
         // 影の中央座標を取得
-        Vector3 center_point = new Vector3();    
+        Vector3 center_point = new Vector3();
         Vector3 dir_center = circle.gameObject.transform.position - gameObject.transform.position;
         Ray ray_center = new Ray(gameObject.transform.position, dir_center);
         RaycastHit[] hits_center = Physics.RaycastAll(ray_center);
@@ -187,7 +236,7 @@ public class CreateShadowCircle : MonoBehaviour
         for (int i = vertex_x.Count - 1; i > -1; i--)
         {
             // 前のX座標と同じ値であれば処理をしない
-            if(i != vertex_x.Count - 1)
+            if (i != vertex_x.Count - 1)
             {
                 if (vertex_x[i] == vertex_x[i + 1])
                 {
@@ -195,7 +244,7 @@ public class CreateShadowCircle : MonoBehaviour
                 }
             }
 
-            foreach(Vector3 temp in vertex)
+            foreach (Vector3 temp in vertex)
             {
                 // 中央の点ははける
                 if (temp == center_point)
